5f1508d9fb154b3e199ce828640bb797b0cc38ad
USER <nurulqalbizahrani11@gmail.com>
2024-04-13 18:14:12 +0800

    php

--- php/signupServer.php
+++ php/signupServer.php
@@ -1,7 +1,22 @@
 <?php
 
-if(isset($_GET['Input'])){
-    $email = $_GET['email'];
-    echo "Email Anda : <b>$email</b>";
-}
+    if(isset($_GET['Input'])){
+        $email = $_GET['email'];
+        echo "Email Anda : <b>$email</b>";
+    }
+   
+    if(isset($_GET['username'])) {
+        $username = $_GET['username'];
+        echo "Nama Anda : <b>$username</b><br>";
+    }
+    if(isset($_GET['email'])) {
+        $email = $_GET['email'];
+        echo "Email Anda : <b>$email</b><br>";
+    }
+    if(isset($_GET['password'])) {
+        $password = $_GET['password'];
+        echo "Password Anda : <b>$password</b><br>";
+    }
+ 
+
 ?>
