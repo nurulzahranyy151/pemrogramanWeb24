73aa7e03037c8bf367bbf36e78ed3825041eebdf
LAERESU <yusril03ibtida@gmail.com>
2024-05-16 20:43:49 +0800

    formpostingan

--- js/masyarakatValidation.js
+++ js/masyarakatValidation.js
@@ -1,5 +1,4 @@
 function validateComment() {
-    // Mendapatkan referensi ke input komentar dan tombol kirim
     var commentInput = document.getElementById('comment');
     var submitBtn = document.getElementById('submit-comment');
     if (commentInput.value.trim() !== '') {
@@ -11,4 +10,40 @@ function validateComment() {
 document.addEventListener('DOMContentLoaded', function() {
     validateComment();
     document.getElementById('comment').addEventListener('input', validateComment);
+});
+
+// Image upload
+const imageIcon = document.querySelector('.bx-image-add');
+const imageUpload = document.getElementById('imageUpload');
+const mediaPreview = document.querySelector('.media-preview');
+const footerReport = document.querySelector('.footer-report');
+
+imageIcon.addEventListener('click', () => {
+    imageUpload.click();
+});
+
+imageUpload.addEventListener('change', event => {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const img = document.createElement('img');
+            img.src = e.target.result;
+            img.alt = 'User Uploaded Image';
+
+            const deleteIcon = document.createElement('i');
+            deleteIcon.classList.add('bx', 'bx-x', 'delete-icon');
+            deleteIcon.addEventListener('click', () => {
+                mediaPreview.removeChild(img);
+                mediaPreview.removeChild(deleteIcon);
+                footerReport.style.marginTop = 0;
+            });
+
+            mediaPreview.innerHTML = ''; 
+            mediaPreview.appendChild(img);
+            mediaPreview.appendChild(deleteIcon);
+            footerReport.style.marginTop = '10px';
+        };
+        reader.readAsDataURL(file);
+    }
 });
